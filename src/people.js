2b2300dabdf94f8d024a55975a70ada16c2789ea
greatness2014 <awsaeed@gmail.com>
2023-10-03 17:23:49 -0600

    initial commit

---
+++ src/people.js
@@ -0,0 +1,15 @@
+export let people = [{
+    name: "John",
+    age: 35,
+    eyeColor: "brown"
+},
+{
+    name: "Joe",
+    age: 36,
+    eyeColor: "black"
+},
+{
+    name: "Jane",
+    age: 40,
+    eyeColor: "blond"
+}];
